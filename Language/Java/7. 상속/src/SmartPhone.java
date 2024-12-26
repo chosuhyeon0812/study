e39e31b19d2948a95d2a402e3653d1d46f694158
조수현 <jeniffer0812@naver.com>
2024-12-26 16:38:36 +0900

    study : 상속, 오버라이딩, 추상클래스 (java)

---
+++ Language/Java/7. 상속/src/SmartPhone.java
@@ -0,0 +1,8 @@
+public class SmartPhone extends Phone {
+    
+    public SmartPhone(String model, String color) {
+        this.model = model;
+        this.color = color;
+        System.out.println("SmartPhone() 생성자 실행");
+    }
+}
---
+++ Language/Java/7. 상속/src/SmartPhoneEx.java
@@ -0,0 +1,7 @@
+public class SmartPhoneEx {
+    public static void main(String[] args) {
+        SmartPhone phone = new SmartPhone("아이폰", "틸");
+        System.out.println(phone.model);
+        System.out.println(phone.color);
+    }
+}
