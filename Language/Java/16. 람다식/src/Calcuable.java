d5a43d5112987d496b5017a625f8a6a13838f019
조수현 <jeniffer0812@naver.com>
2025-01-08 15:05:11 +0900

    study: 람다식 vs 메소드 참조 (java)

---
+++ Language/Java/16. 람다식/src/Calcuable.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+public interface Calcuable {
+    double calc (double x, double y);
+}
---
+++ Language/Java/16. 람다식/src/Calculable.java
@@ -0,0 +1,6 @@
+@FunctionalInterface
+public interface Calculable {
+    // 추상 메소드
+    void calculate(int x, int y);
+}
+
